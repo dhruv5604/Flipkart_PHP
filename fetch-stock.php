e0a5ba1ffc86e0a799a918f1cf57337cfa35cfb4
dhruv5604 <dhruvsolanki5604@gmail.com>
2025-04-01 16:39:55 +0530

    if stock is less than quantity of product in cart then user can't increase quantity

---
+++ fetch-stock.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('display_errors',1);
+require('connection.php');
+require('check_post.php');
+
+$cart_id = $_POST['cart_id'];
+
+$query = "select * from inventory i join cart c on i.product_id = c.product_id where c.id = ?";
+$stmt = $con->prepare($query);
+$stmt->bind_param("i",$cart_id);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$stock = [];
+
+while($row = $result->fetch_assoc()){
+    $stock[] = $row;
+}
+
+echo json_encode($stock);
